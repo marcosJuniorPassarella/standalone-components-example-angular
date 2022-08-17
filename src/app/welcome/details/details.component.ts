944714a3a3922719691ddd93c2a296d1a34adbd2
MJWORKS <mjworks2020@outlook.com>
2022-08-17 12:41:13 -0300

    Transformated the welcome cmpnt in standalone

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 import { AnalyticsService } from "src/app/shared/analytics.service";
-import { SharedModule } from "../../shared/shared.module";
+import { HighlightDirective } from "../../shared/highlight.directive";
 
 @Component({
   standalone: true,
-  imports: [SharedModule],
+  imports: [HighlightDirective],
   selector: "app-details",
   templateUrl: "./details.component.html",
   styleUrls: ["./details.component.css"],
