6e02ba3e8fba8d24ef501f6756cada479ff522ae
MJWORKS <mjworks2020@outlook.com>
2022-08-17 13:28:19 -0300

    Added routes in the standalone aplication

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { enableProdMode } from "@angular/core";
+import { enableProdMode, importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { environment } from "./environments/environment";
 import { AppComponent } from "./app/app.component";
+import { AppRoutingModule } from "./app/app-routing.module";
 
 if (environment.production) {
   enableProdMode();
 }
 
 // provê o BrowsersModule para a aplicação, tirando a necessidade de importá-lo
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  // torna o app component ciente das rotas da aplicação
+  providers: [importProvidersFrom(AppRoutingModule)],
+});
